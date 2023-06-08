8d074a996a0ab555604ece5643a5b5863615651b
Kyra Atkinson <kyraatkinson@Kyras-Air-2.home>
2023-06-08 00:46:53 -0400

    update

--- puppy-assign/src/App.jsx
+++ puppy-assign/src/App.jsx
@@ -1,25 +1,45 @@
+import {puppyList} from './data.js'
+import { useState } from 'react'
+import './App.css'
+
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [featPupId, setFeatPupId] = useState(null)
+  const [puppies, setPuppies] = useState(puppyList)
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  console.log(featuredPup)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+ 
+    return (
+      <div className='App'>
+        {
+          puppies.map((puppy) => {
+            return (
+            <p class="pup" onClick = {() => { setFeatPupId(puppy.id)}} key={puppy.id}>
+              {puppy.name}
+              </p>
+             
+            )
+          })
+        } 
+
+{featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+        
       </div>
-      <h1>Vite + React</h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+
+    )
+      }
+
+
 
 export default App
